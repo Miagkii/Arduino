8ede6419292cfdb258d5d3c1d27d2840ee148d4e
Andrei Miagkii <andreimiagkii@gmail.com>
2024-08-31 11:51:29 -0400

    Update Check_button_plotter.ino
    placed the code for checking button state in the structure function.

--- Sketches/Check_button_plotter/Check_button_plotter.ino
+++ Sketches/Check_button_plotter/Check_button_plotter.ino
@@ -1,27 +1,21 @@
 #include <EEPROM.h>
 const byte Setbutton = 2;
-unsigned long startMillis, exitMillis;
-bool setClick = false;
-bool setPush = false;
-bool startKey = false;
-bool endKey = false;
-bool pushKey = false;
+unsigned long exitMillis;
 bool exitKey = false;
-byte setState = 1;
-bool b = false;
-int a = 1;
+bool inputLevel = false;
+int listLevel = 1;
 int wayClick = 0;
 int wayPush = 0;
 
-struct sTime{
+struct buttons{
   unsigned long sMillis;
   byte state;
-  bool startK;
-  bool endK;
-  bool pushK;
-} set, t, f;
-
-
+  bool startKey;
+  bool endKey;
+  bool pushKey;
+  bool click;
+  bool push;
+} set, temp, function;
 
 void setup() {
   Serial.begin(9600);
@@ -29,59 +23,14 @@ void setup() {
 }
 
 void loop() {
-  setClick = false;
-  setPush = false;
+  set.click = false;
+  set.push = false;
   set.state = digitalRead(Setbutton);
-  t = set;
-  set = start(t);
-  
-  /*
-  if (startKey == false && setState == 0) {   // Start time press buttom
-    startMillis = millis();  
-    startKey = true;
-    endKey = false;
-    pushKey = true;
-    Serial.print("Start: ");
-    Serial.println(startMillis);
-  }
-  
-  if (startKey == true && setState == 1){   // End time press buttom
-    endMillis = millis();
-    endKey = true;
-    startKey = false;
-    pushKey = false;
-    Serial.print("End: ");
-    Serial.println(endMillis);
-  }
-  */
-  if (set.startK == true && set.state == 1){   // End time press buttom
-    set.sMillis = millis() - set.sMillis;
-    set.endK = true;
-    set.startK = false;
-    set.pushK = false;
-    Serial.print("duration: ");
-    Serial.println(set.sMillis);
-  }
-  
-  if (set.endK == true && set.sMillis>10 && set.sMillis<3000){    //Click button
-    setClick = true;
-    set.endK = false;
-    Serial.println(set.sMillis);
-    Serial.println("setClick");
-    
-  } 
-  if (set.pushK == true && (millis() - set.sMillis)>3000) {    //Push button more 3 sec
-    setPush = true;
-    set.pushK = false;
+  temp = set;
+  set = stateCheck(temp);
    
-    Serial.print("duration: ");
-    Serial.println(millis() - set.sMillis);
-    Serial.println("setPush");
-  }
-  
-  
-  if (b == false){
-    switch (a) {
+  if (inputLevel == false){
+    switch (listLevel) {
       case 1:
         Serial.println("temp");
         wayClick = 2;
@@ -105,26 +54,26 @@ void loop() {
     }
   }
   else {
-    inputDigits(a);
+    inputDigits(listLevel);
   }
   
 
 }
 
 void exit() {
-  if (exitKey == false || setClick == true || setPush == true) {
+  if (exitKey == false || set.click == true || set.push == true) {
     exitKey = true;
     exitMillis = millis();
   }
-  if (b == true && setClick == true){
-    b = false;
+  if (inputLevel == true && set.click == true){
+    inputLevel = false;
     
     exitKey = false;
   }
   
   if ((millis() - exitMillis)>5000){
-    b = false;
-    a = 1;
+    inputLevel = false;
+    listLevel = 1;
     exitKey = false;
   }
   
@@ -133,24 +82,24 @@ void exit() {
 
 
 void checkClick(int c){
-  if (setClick == true){
-    a = c; 
+  if (set.click == true){
+    listLevel = c; 
   }
-  setClick = false;
+  set.click = false;
 }
 
 void checkPush(int p){
-  if (setPush == true){
-    a = p; 
+  if (set.push == true){
+    listLevel = p; 
   }
-  setPush = false;
+  set.push = false;
 }
 
 void checkInput() {
-    if (setClick == true){
-    b = true; 
+    if (set.click == true){
+    inputLevel = true; 
   }
-  setClick = false;
+  set.click = false;
 }
 
 void inputDigits(int address){
@@ -161,27 +110,37 @@ void inputDigits(int address){
   exit();
 }
 
-sTime start(sTime f){
-  if (f.startK == false && f.state == 0){
-    f.sMillis = millis();
-    f.startK = true;
-    f.endK = false;
-    f.pushK = true;
+buttons stateCheck(buttons function){
+  if (function.startKey == false && function.state == 0){ // Start time press button
+    function.sMillis = millis();
+    function.startKey = true;
+    function.endKey = false;
+    function.pushKey = true;
     Serial.print("Start: ");
-    Serial.println(f.sMillis);
+    Serial.println(function.sMillis);
   }
-  return f;
-}
-/*
-void start(byte set){
-if (startKey == false && set == 0) {   // Start time press buttom
-    startMillis = millis();  
-    startKey = true;
-    endKey = false;
-    pushKey = true;
-    Serial.print("Start: ");
-    Serial.println(startMillis);
+    if (function.startKey == true && function.state == 1){   // End time press buttom
+    function.sMillis = millis() - function.sMillis;
+    function.endKey = true;
+    function.startKey = false;
+    function.pushKey = false;
+    Serial.print("duration1: ");
+    Serial.println(function.sMillis);
   }
   
+  if (function.endKey == true && function.sMillis>10 && function.sMillis<3000){    // Check click button
+    function.click = true;
+    function.endKey = false;
+    Serial.println(function.sMillis);
+    Serial.println("seftClick");
+    
+  } 
+  if (function.pushKey == true && (millis() - function.sMillis)>3000) {    // Check push button more 3 sec
+    function.push = true;
+    function.pushKey = false;
+    Serial.print("duration2: ");
+    Serial.println(millis() - function.sMillis);
+    Serial.println("setPush");
+  }
+  return function;
 }
-*/
